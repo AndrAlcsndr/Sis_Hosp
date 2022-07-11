ebdc9e9ca13a62e506c3ec5d284e42faaca9bcd7
André Durante Jr <108413053+Andredurantejr@users.noreply.github.com>
2022-07-11 09:16:31 -0300

    Update entidades.h

--- Headers/entidades.h
+++ Headers/entidades.h
@@ -1,146 +1,139 @@
+//Trabalho de TP 1 - André Fioravante Nicolodi Durante Junior 16/0112010 & André Alecsander Araújo Lopes 18/0116142 
+
 #ifndef ENTIDADES_H_INCLUDED
 #define ENTIDADES_H_INCLUDED
 
-#include "dominios.h"
+#include <iostream>
+#include <iomanip>
+#include <math.h>
 #include <string>
+#include <bits/stdc++.h>
+
+#include "dominios.h"
 
 using namespace std;
 
-/* -----------------------------------------------------------------------
-// Estrutura de c�digo sugerida para declara��o de classe entidade.
-// Substituir Entidade por nome da classe.
-// Substituir Dominio.
-// Substituir nomeAtributo.
-
-class Entidade {
-    private:
-            Dominio nomeAtributo;                   // Declarar cada atributo da classe.
-    public:
-            void setnomeAtributo(const Dominio&);   // Declarar m�todo set para cada atributo.
-            Dominio getnomeAtributo() const;        // Declarar m�todo get para cada atributo.
-};
-
-inline void Entidade::setnomeAtributo(const Dominio &nomeAtributo){
-    this->nomeAtributo = nomeAtributo;
-}
-
-inline Dominio Entidade::getnomeAtributo() const{
-    return nomeAtributo;
-}
-
------------------------------------------------------------------------ */
-
-// -----------------------------------------------------------------------
-//Declaração de classe USUARIO e implementações de métodos
-
-class Usuario
-{
-    private:
-        Nomes nome;
-        Email endereco;                            //Primary Key
-        Password senha;
-        Idioma idioma;
-        Aniversario aniversario;
-        Descricao descricao;
-
-    public:
-        Codigo listarHospedagens();                 //Listar hospedagens do Usuário
-        Codigo getHospedagens(Codigo codigo) ;      //Realizar consulta de uma hospedagem específica do Usuário   
-        Prioridade getPrioridade() const;           // ?
-};
-
-inline Codigo Usuario::listarHospedagens()
-{
-    return Codigo listaDeHospedagens;               //Retorna uma lista de hospedagens
-
-}
-
-inline Codigo Usuario::getHospedagens(Codigo codigo)
-{
-    return hospedagem;                              //Retorna uma hospedagem específica com base no código enviado
-
-}
-
-// -----------------------------------------------------------------------
-
-// -----------------------------------------------------------------------
-//Declaração de classe HOSPEDAGEM e implementações de métodos
-
-class Hospedagem
-{
-    private:
-        Codigo codigo;                              //Primary Key
-        string cidade;
-        string pais;
-        float nota;
-    
-    public:
-        Codigo setHospedagem ();                    //Realiza a criação de uma hospedagem, que retorna um código de criação
-        Codigo getHospedagem (Codigo codigo);       //Realiza a consulta de uma hospedagem específica, retornando um nome e um código
-        float getNotaHospedagem (Codigo codigo);    //Realiza a consulta de uma hospedagem específica passando o código do hotel
-        string getAnfitrioes();                     //Realiza consulta de Usuários hospedados no hotel
-        float getHospedes();                        //Realiza uma consulta de todos os usuários hospedados naquele hotel, retornando uma lista
-};
-
-inline Codigo Hospedagem::setHospedagem()
-{
-    Codigo hospedagem;
-    return hospedagem;                              //Cria uma hospedagem específica com base no código enviado
-
-}
-
-inline Codigo Hospedagem::getHospedagem(Codigo codigo)
-{
-    Codigo hospedagem;
-    return hospedagem;                              //Cria uma hospedagem específica com base no código enviado
-
-}
-
-inline string Hospedagem::getAnfitrioes()
-{
-    using std:: string;
-    string anfitrioes[] = { "anfitriao1", "anfitriao2",
-                                "anfitriao3",  "anfitriao4"};
-    return anfitrioes;                              //Cria uma hospedagem específica com base no código enviado
-
-}
-
-inline string Hospedagem::getAnfitrioes()
-{
-    using std:: string;
-    string anfitrioes[] = { "anfitriao1", "anfitriao2",
-                                "anfitriao3",  "anfitriao4"};
-    return anfitrioes;                              //Cria uma hospedagem específica com base no código enviado
-
-}
-// -----------------------------------------------------------------------
-
-// -----------------------------------------------------------------------
-//Declaração de classe AVALIAÇÃO e implementações de métodos
-
-class Avaliacao
-{
+class Usuario {
 private:
-   Codigo codigo;                                   //Primary Key
-   Nota nota;
-   Descricao descricao;
+
+Nome nome;
+Email email;
+Senha senha;
+Idioma idioma;
+Data data; 
+Descricao descricao;
 
 public:
-    float getAvaliacoes ();
+void setNome(const Nome&);
+void setEmail(const Email&);
+void setSenha(const Senha&);
+void setIdioma(const Idioma&);
+void setData(const Data&);
+void setDescricao(const Descricao&);
+
+Nome getNome() const;
+Email getEmail() const;
+Senha getSenha() const;
+Idioma getIdioma() const;
+Data getData() const;
+Descricao getDescricao() const;
 
 };
 
+// -----------------------------------------------------
+// Implementacao Metodos inline set da entidade Usuario.
+// -----------------------------------------------------
 
+inline void Usuario::setNome(const Nome& nome){
+this->nome = nome;
+}
+inline void Usuario::setEmail(const Email& email){
+this->email = email;
+}
+inline void Usuario::setSenha(const Senha& senha){
+this->senha = senha;
+}
+inline void Usuario::setIdioma(const Idioma& idioma){
+this->idioma = idioma;
+}
+inline void Usuario::setData(const Data& data){
+this->data = data;
+}
+inline void Usuario::setDescricao(const Descricao& descricao){
+this->descricao = descricao;
+}
+// -----------------------------------------------------
+// Implementacao Metodos inline get da entidade Usuario.
+// -----------------------------------------------------
 
-
-inline void Projeto::setPrioridade(const Prioridade &prioridade)
-{
-    this->prioridade = prioridade;
+inline Nome Usuario::getNome() const {
+return nome;
 }
 
-inline Prioridade Projeto::getPrioridade() const
-{
-    return prioridade;
+inline Email Usuario::getEmail() const {
+return email;
 }
 
-#endif // ENTIDADES_H_INCLUDED
+inline Senha Usuario::getSenha() const {
+return senha;
+}
+
+inline Idioma Usuario::getIdioma() const {
+return idioma;
+}
+
+inline Data Usuario::getData() const {
+return data;
+}
+
+inline Descricao Usuario::getDescricao() const {
+return descricao;
+}
+
+
+class Hospedagem{
+private:
+
+public:
+
+}
+
+
+class Avaliacao {
+private:
+
+Codigo codigo;
+Nota nota;
+Descricao descricao;
+public:
+void setCodigo(const Codigo&);
+void setNota(const Nota&);
+void setDescricao(const Descricao&);
+
+Codigo getCodigo() const;
+Nota getNota() const;
+Descricao getDescricao() const;
+};
+
+inline void Avaliacao::setCodigo(const Codigo& codigo){
+this->codigo = codigo;
+}
+inline void Avaliacao::setDescricao(const Descricao& descricao){
+this->descricao = descricao;
+}
+inline void Avaliacao::setNota(const Nota& nota){
+this->nota = nota;
+}
+// -------------------------------------------------------
+// Implementacao Metodos inline get da entidade Avaliacao.
+// -------------------------------------------------------
+
+inline Codigo Avaliacao::getCodigo() const {
+return codigo;
+}
+inline Nota Avaliacao::getNota() const {
+return nota;
+}
+inline Descricao Avaliacao::getDescricao() const {
+return descricao;
+}
